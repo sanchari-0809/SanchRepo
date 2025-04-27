58d34bee3ace6e3eb843f5547843b22450d249b9
sanchari-0809 <114421042+sanchari-0809@users.noreply.github.com>
2025-04-27 18:58:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require("http");
+
+// Create a simple server
+const server = http.createServer((req, res) => {
+  res.statusCode = 200; // OK
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello, World!\n");
+});
+
+// Set the port
+const PORT = 4000;
+
+// Start the server
+server.listen(PORT, () => {
+  console.log(Server running at http://localhost:${PORT});
+});
